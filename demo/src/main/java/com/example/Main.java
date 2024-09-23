69ffa676f4fb9d853e0c6712b9b355d031819e8a
Julia <julia.sanchez@gruposantander.com>
2024-09-23 20:18:09 +0200

    Enhanced greeting

--- demo/src/main/java/com/example/Main.java
+++ demo/src/main/java/com/example/Main.java
@@ -6,11 +6,11 @@ import java.time.LocalDateTime;
 public class Main {
     public static void main(String[] args) {
         LocalDateTime locaDate = LocalDateTime.now();
-int horas  = locaDate.getHour();
-int minutos = locaDate.getMinute();
-int segundos = locaDate.getSecond();
-System.out.println("Hora actual : " + horas  + ":"+ minutos +":"+segundos);
-        System.out.println("Hello world!");
+        int horas  = locaDate.getHour();
+        int minutos = locaDate.getMinute();
+        int segundos = locaDate.getSecond();
+        System.out.println("Hora actual : " + horas  + ":"+ minutos +":"+segundos);
+        System.out.println("Hello world!!!!!!!!!!!!!!!");
         System.out.println("Please, introduce your name:");
         Scanner scanner = new Scanner(System.in);
         String name = scanner.nextLine();
