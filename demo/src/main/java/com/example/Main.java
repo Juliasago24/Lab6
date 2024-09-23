592aaeceb08fe9e5d781da736657c224b9821a0f
Julia <julia.sanchez@gruposantander.com>
2024-09-23 18:49:46 +0200

    Personalized greeting

--- demo/src/main/java/com/example/Main.java
+++ demo/src/main/java/com/example/Main.java
@@ -1,7 +1,12 @@
 package com.example;
+import java.util.Scanner; //Will be used later
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("Please, introduce your name:");
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+        System.out.println("Hi "+name+"!");
     }
 }
